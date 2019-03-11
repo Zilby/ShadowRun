0814d32efd36193691ebd24629a8eef46bf092ee
svogel <svogel@ccs.neu.edu>
2019-03-11 14:59:52 -0400

    Started work on feed panel

---
+++ ShadowRun/Assets/Scripts/FeedMessageView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityWeld.Binding;
+
+[Binding]
+public class FeedMessageView : DataBindObject
+{
+    private string sender;
+    [Binding]
+    public string Sender
+    {
+        get { return sender;}
+        private set { SetProperty(ref sender, value, nameof(Sender));}
+    }
+
+    private string text;
+    [Binding]
+    public string Text
+    {
+        get { return text;}
+        private set { SetProperty(ref text, value, nameof(Text));}
+    }
+    
+    public FeedMessageView(string sender, string text)
+    {
+        Sender = sender;
+        Text = text;
+    }
+}
---
+++ ShadowRun/Assets/Scripts/FeedPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityWeld;
+using UnityWeld.Binding;
+
+[Binding]
+public class FeedPanel : Panel
+{
+    private ObservableList<FeedMessageView> messages;
+    [Binding]
+    public ObservableList<FeedMessageView> Messages
+    {
+        get { return messages; }
+        set { SetProperty(ref messages, value, nameof(Messages)); }
+    }
+}
