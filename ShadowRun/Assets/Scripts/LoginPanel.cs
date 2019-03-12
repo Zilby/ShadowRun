471be397f658376e41a5477254afe63696e8c80c
Michael Chisholm <macchiz124@gmail.com>
2019-03-11 22:12:14 -0400

    fixed issues from merge

--- ShadowRun/Assets/Scripts/LoginPanel.cs
+++ ShadowRun/Assets/Scripts/LoginPanel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoginPanel : MonoBehaviour
+public class LoginPanel : Panel
 {
     // Start is called before the first frame update
     void Start()
