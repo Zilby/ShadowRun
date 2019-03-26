ff383b417de42eb52c7773ac83d2abd511df656c
DecidingToast <svogel2bro@gmail.com>
2019-03-26 11:18:29 -0400

    Added shake support

--- ShadowRun/Assets/Scripts/DiceRollPanel.cs
+++ ShadowRun/Assets/Scripts/DiceRollPanel.cs
@@ -62,6 +62,31 @@ public class DiceRollPanel : Panel
         diceRoller.ResetRoller();
     }
 
+    float accelerometerUpdateInterval = 1.0f / 60.0f;
+    float lowPassKernelWidthInSeconds = 1.0f;
+    float shakeDetectionThreshold = 2.0f;
+    float lowPassFilterFactor;
+    Vector3 lowPassValue;
+
+    void Start()
+    {
+        lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+        shakeDetectionThreshold *= shakeDetectionThreshold;
+        lowPassValue = Input.acceleration;
+    }
+
+    void Update()
+    {
+        Vector3 acceleration = Input.acceleration;
+        lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
+        Vector3 deltaAcceleration = acceleration - lowPassValue;
+
+        if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
+        {
+            RollDice();
+        }
+    }
+
     private IEnumerator WaitForResult()
     {
         diceRoller.RollDice();
